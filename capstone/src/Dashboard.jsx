6e1cee23f0b2b61d1aa13b8c0776f0ffbb54947d
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-07-17 17:50:32 +0800

    Table
    Added table for electornics

--- capstone/src/Dashboard.jsx
+++ capstone/src/Dashboard.jsx
@@ -1,25 +1,26 @@
 export default function Dashboard() {
+
     return (
       <div className="flex justify-center items-center h-screen bg-gray-100">
         <div className="text-center">
           <h1 className="text-3xl font-bold mb-8">Stocks and Inventory</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20">
-            <button className="bg-black text-white py-20 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button className="bg-black text-white py-20 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl"  onClick={()=>window.location.href="/Electronics" }>
               Electronics
             </button>
-            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl">
               Furniture
             </button>
-            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl">
               IT Equipment
             </button>
-            <button onClick={() => window.location.href = "/sample"} className="bg-black text-white py-20 px-20   rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button onClick={() => window.location.href = "/sample"} className="bg-black text-white py-20 px-20   rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl">
               Office Supplies
             </button>
-            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl">
               Maintenance Supplies
             </button>
-            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300">
+            <button className="bg-black text-white py-8 px-16 rounded-lg shadow-md hover:bg-gray-800 transition duration-300 text-3xl">
               Consumables
             </button>
           </div>
---
+++ capstone/src/Tables/Electornics.jsx
@@ -0,0 +1,31 @@
+export default function Electronics()
+{
+    return <div class="flex justify-center items-center h-screen ">
+            <table class="table-auto border-collapse border border-slate-500">
+        <thead>
+            <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+            </tr>
+            <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+            </tr>
+            <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+}
