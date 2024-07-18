20790f576413d39909ed81c51841ca96249d44d7
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-07-18 15:02:38 +0800

    Bag o

--- capstone/src/Tables/Electornics.jsx
+++ capstone/src/Tables/Electornics.jsx
@@ -15,26 +15,24 @@ export default function Electronics()
     <th class="column5">Description</th>
     <th class="column6">Date</th>
     <th class="column6">Operation</th>
+    
+
     </tr>
     </thead>
     <tbody>
   
     <tr>
-    <td class="column1">2017-09-26 05:57</td>
+    <td class="column1">2</td>
     <td class="column2">200396</td>
     <td class="column3">Game Console Controller</td>
     <td class="column4">$22.00</td>
     <td class="column5">2</td>
-    <td class="column6">$44.00</td>
+    <td class="column6">2017-09-26 05:57</td>
+    <td class="flex items-center justify-center mt-2"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "><i class="fa-solid fa-plus"></i></button>
+    <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "><i class="fa-solid fa-pen"></i></button>
+    <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"><i class="fa-solid fa-trash"></i></button></td>
     </tr>
-    <tr>
-    <td class="column1">2017-09-25 23:06</td>
-    <td class="column2">200392</td>
-    <td class="column3">USB 3.0 Cable</td>
-    <td class="column4">$10.00</td>
-    <td class="column5">3</td>
-    <td class="column6">$30.00</td>
-    </tr>
+
     <tr>
     <td class="column1">2017-09-24 05:57</td>
     <td class="column2">200391</td>
