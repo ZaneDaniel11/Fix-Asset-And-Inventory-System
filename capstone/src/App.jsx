72019e13f098de4d601ad4fd41c01394e6b59292
Zane Daniel <118499857+yawaka21@users.noreply.github.com>
2024-07-18 16:14:21 +0800

    Clean

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,20 +1,19 @@
-import Dashboard from "./Dashboard"
-import './app.css'
-import { Route, Routes } from "react-router-dom"
-import Sample from './sample.jsx'
-import Electronics from "./Tables/Electornics.jsx"
+import Dashboard from "./Dashboard";
+import "./app.css";
+import { Route, Routes } from "react-router-dom";
+import Sample from "./sample.jsx";
+import Electronics from "./Tables/Electornics.jsx";
 
 function App() {
-
   return (
     <div>
-    <Routes>
-    <Route exact path="/Electronics" element={<Electronics />}/>
-    <Route exact path="/" element={<Dashboard />} />
-    <Route exact path="/sample" element={<Sample />} />
-    </Routes>
+      <Routes>
+        <Route exact path="/Electronics" element={<Electronics />} />
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/sample" element={<Sample />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- capstone/src/Tables/Electornics.jsx
+++ capstone/src/Tables/Electornics.jsx
@@ -1,101 +1,113 @@
-export default function Electronics()
-{
-    return <>
-    <div class="limiter">
-    <div class="container-table100">
-    <div class="wrap-table100">
-    <div class="table100">
-    <table>
-    <thead>
-    <tr class="table100-head">
-    <th class="column1">Item ID</th>
-    <th class="column2">Item Name</th>
-    <th class="column3">Quantity</th>
-    <th class="column4">UnitPrice</th>
-    <th class="column5">Description</th>
-    <th class="column6">Date</th>
-    <th class="column6">Operation</th>
-    
-
-    </tr>
-    </thead>
-    <tbody>
-  
-    <tr>
-    <td class="column1">2</td>
-    <td class="column2">200396</td>
-    <td class="column3">Game Console Controller</td>
-    <td class="column4">$22.00</td>
-    <td class="column5">2</td>
-    <td class="column6">2017-09-26 05:57</td>
-    <td class="flex items-center justify-center mt-2"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "><i class="fa-solid fa-plus"></i></button>
-    <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "><i class="fa-solid fa-pen"></i></button>
-    <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"><i class="fa-solid fa-trash"></i></button></td>
-    </tr>
-
-    <tr>
-    <td class="column1">2017-09-24 05:57</td>
-    <td class="column2">200391</td>
-    <td class="column3">Smartwatch 4.0 LTE Wifi</td>
-    <td class="column4">$199.00</td>
-    <td class="column5">6</td>
-    <td class="column6">$1494.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-23 05:57</td>
-    <td class="column2">200390</td>
-    <td class="column3">Camera C430W 4k</td>
-    <td class="column4">$699.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$699.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-22 05:57</td>
-    <td class="column2">200389</td>
-    <td class="column3">Macbook Pro Retina 2017</td>
-    <td class="column4">$2199.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$2199.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-21 05:57</td>
-    <td class="column2">200388</td>
-    <td class="column3">Game Console Controller</td>
-    <td class="column4">$999.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$999.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-19 05:57</td>
-    <td class="column2">200387</td>
-    <td class="column3">iPhone X 64Gb Grey</td>
-    <td class="column4">$999.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$999.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-18 05:57</td>
-    <td class="column2">200386</td>
-    <td class="column3">iPhone X 64Gb Grey</td>
-    <td class="column4">$999.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$999.00</td>
-    </tr>
-    <tr>
-    <td class="column1">2017-09-22 05:57</td>
-    <td class="column2">200389</td>
-    <td class="column3">Macbook Pro Retina 2017</td>
-    <td class="column4">$2199.00</td>
-    <td class="column5">1</td>
-    <td class="column6">$2199.00</td>
-    </tr>
-
-    </tbody>
-    </table>
-    </div>
-    </div>
-    </div>
-    </div>
+export default function Electronics() {
+  return (
+    <>
+      <div class="limiter">
+        <div class="container-table100">
+          <div class="wrap-table100">
+            <div class="table100">
+              <table>
+                <thead>
+                  <tr class="table100-head">
+                    <th class="column1">Item ID</th>
+                    <th class="column2">Item Name</th>
+                    <th class="column3">Quantity</th>
+                    <th class="column4">UnitPrice</th>
+                    <th class="column5">Description</th>
+                    <th class="column6">Date</th>
+                    <th class="column6">Operation</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="column1">2</td>
+                    <td class="column2">200396</td>
+                    <td class="column3">Game Console Controller</td>
+                    <td class="column4">$22.00</td>
+                    <td class="column5">2</td>
+                    <td class="column6">2017-09-26 05:57</td>
+                    <td class="flex items-center justify-center mt-2">
+                      <button
+                        type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
+                      >
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
+                      >
+                        <i class="fa-solid fa-pen"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
+                      >
+                        <i class="fa-solid fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="column1">2</td>
+                    <td class="column2">200396</td>
+                    <td class="column3">Game Console Controller</td>
+                    <td class="column4">$22.00</td>
+                    <td class="column5">2</td>
+                    <td class="column6">2017-09-26 05:57</td>
+                    <td class="flex items-center justify-center mt-2">
+                      <button
+                        type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
+                      >
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
+                      >
+                        <i class="fa-solid fa-pen"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
+                      >
+                        <i class="fa-solid fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="column1">2</td>
+                    <td class="column2">200396</td>
+                    <td class="column3">Game Console Controller</td>
+                    <td class="column4">$22.00</td>
+                    <td class="column5">2</td>
+                    <td class="column6">2017-09-26 05:57</td>
+                    <td class="flex items-center justify-center mt-2">
+                      <button
+                        type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
+                      >
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
+                      >
+                        <i class="fa-solid fa-pen"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
+                      >
+                        <i class="fa-solid fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-    
+  );
 }
