e9270d4c03d491ff0e927ea670da951f69e5259c
Zane Daniel <118499857+yawaka21@users.noreply.github.com>
2024-07-19 15:16:37 +0800

    added new

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -3,11 +3,13 @@ import "./app.css";
 import { Route, Routes } from "react-router-dom";
 import Sample from "./sample.jsx";
 import Electronics from "./Tables/Electornics.jsx";
+import Login from "./Login.jsx";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route exact path="/Login" element={<Login />} />
         <Route exact path="/Electronics" element={<Electronics />} />
         <Route exact path="/" element={<Dashboard />} />
         <Route exact path="/sample" element={<Sample />} />
