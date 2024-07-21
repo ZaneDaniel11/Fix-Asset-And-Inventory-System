7d110f62da400f6dd75bebb8fbc2cec74b2d5e4a
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-07-21 23:09:15 +0800

    added new feature

--- capstone/src/Tables/Electornics.jsx
+++ capstone/src/Tables/Electornics.jsx
@@ -1,104 +1,57 @@
+import React, { useState } from "react";
+
 export default function Electronics() {
+  const [add_modal, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <>
-      <div class="limiter">
-        <div class="container-table100">
-          <div class="wrap-table100">
-            <div class="table100">
+      <div className="limiter">
+        <div className="container-table100">
+          <div className="wrap-table100">
+            <div className="table100">
               <table>
                 <thead>
-                  <tr class="table100-head">
-                    <th class="column1">Item ID</th>
-                    <th class="column2">Item Name</th>
-                    <th class="column3">Quantity</th>
-                    <th class="column4">UnitPrice</th>
-                    <th class="column5">Description</th>
-                    <th class="column6">Date</th>
-                    <th class="column6">Operation</th>
+                  <tr className="table100-head">
+                    <th className="column1">Item ID</th>
+                    <th className="column2">Item Name</th>
+                    <th className="column3">Quantity</th>
+                    <th className="column4">UnitPrice</th>
+                    <th className="column5">Description</th>
+                    <th className="column6">Date</th>
+                    <th className="column6">Operation</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td class="column1">2</td>
-                    <td class="column2">200396</td>
-                    <td class="column3">Game Console Controller</td>
-                    <td class="column4">$22.00</td>
-                    <td class="column5">2</td>
-                    <td class="column6">2017-09-26 05:57</td>
-                    <td class="flex items-center justify-center mt-2">
+                    <td className="column1">2</td>
+                    <td className="column2">200396</td>
+                    <td className="column3">Game Console Controller</td>
+                    <td className="column4">$22.00</td>
+                    <td className="column5">2</td>
+                    <td className="column6">2017-09-26 05:57</td>
+                    <td className="flex items-center justify-center mt-2">
                       <button
                         type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
+                        onClick={openModal}
+                        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2"
                       >
-                        <i class="fa-solid fa-plus"></i>
+                        <i className="fa-solid fa-plus"></i>
                       </button>
                       <button
                         type="button"
-                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
+                        onClick={openModal}
+                        className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2"
                       >
-                        <i class="fa-solid fa-pen"></i>
+                        <i className="fa-solid fa-pen"></i>
                       </button>
                       <button
                         type="button"
-                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
+                        className="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2"
                       >
-                        <i class="fa-solid fa-trash"></i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="column1">2</td>
-                    <td class="column2">200396</td>
-                    <td class="column3">Game Console Controller</td>
-                    <td class="column4">$22.00</td>
-                    <td class="column5">2</td>
-                    <td class="column6">2017-09-26 05:57</td>
-                    <td class="flex items-center justify-center mt-2">
-                      <button
-                        type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
-                      >
-                        <i class="fa-solid fa-plus"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
-                      >
-                        <i class="fa-solid fa-pen"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
-                      >
-                        <i class="fa-solid fa-trash"></i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="column1">2</td>
-                    <td class="column2">200396</td>
-                    <td class="column3">Game Console Controller</td>
-                    <td class="column4">$22.00</td>
-                    <td class="column5">2</td>
-                    <td class="column6">2017-09-26 05:57</td>
-                    <td class="flex items-center justify-center mt-2">
-                      <button
-                        type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none "
-                      >
-                        <i class="fa-solid fa-plus"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 "
-                      >
-                        <i class="fa-solid fa-pen"></i>
-                      </button>
-                      <button
-                        type="button"
-                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:"
-                      >
-                        <i class="fa-solid fa-trash"></i>
+                        <i className="fa-solid fa-trash"></i>
                       </button>
                     </td>
                   </tr>
@@ -108,6 +61,66 @@ export default function Electronics() {
           </div>
         </div>
       </div>
+
+      {add_modal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg max-w-lg w-full mx-4 md:mx-0">
+            <div className="flex justify-between items-center">
+              <h5 className="text-lg font-semibold">Modal Title</h5>
+              <button
+                type="button"
+                onClick={closeModal}
+                className="text-gray-500 hover:text-gray-700"
+              >
+                <i className="fa-solid fa-xmark"></i>
+              </button>
+            </div>
+            <form action="">
+              <div className="mt-4">
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+                  <input
+                    className="bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Item Name"
+                  />
+                  <input
+                    className="bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="text"
+                    placeholder="Quantity"
+                  />
+                  <input
+                    className="bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                    type="email"
+                    placeholder="Unit Price"
+                  />
+                </div>
+                <div className="my-4">
+                  <textarea
+                    placeholder="Description"
+                    className="h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline w-full"
+                  ></textarea>
+                </div>
+              </div>
+
+              <div className="flex justify-end mt-4">
+                <button
+                  type="button"
+                  onClick={closeModal}
+                  className="bg-gray-200 text-gray-800 px-4 py-2 rounded-lg mr-2"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                >
+                  Save changes
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </>
   );
 }
